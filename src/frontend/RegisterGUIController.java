a22aeb70354171dcaf303a07094ecdc4b0a6f662
anthonysaka <48063344+anthonysaka@users.noreply.github.com>
2020-03-20 17:28:08 -0400

    Class Controller Register. This class is for controller register view.
    Signed-off-by: Anthony Sakamoto <48063344+anthonysaka@users.noreply.github.com>

---
+++ src/frontend/RegisterGUIController.java
@@ -0,0 +1,39 @@
+package frontend;
+
+import com.jfoenix.controls.JFXButton;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * @developed_by Anthony Sakamoto
+ *
+ */
+public class RegisterGUIController {
+
+    @FXML
+    private JFXButton btnSignUp;
+
+    @FXML
+    private JFXButton btnClose;
+    
+    @FXML
+    private AnchorPane anchorPaneRoot;
+
+    
+    
+    
+    
+    /*** METHODS ***/
+    @FXML
+    void close(ActionEvent event) {
+
+    }
+
+    @FXML
+    void setRegister(ActionEvent event) {
+
+    }
+
+}
