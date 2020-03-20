2b41573eadb0fac3518a014ced0a15a5b7e00f6b
anthonysaka <48063344+anthonysaka@users.noreply.github.com>
2020-03-19 22:54:33 -0400

    Created Register GUI

---
+++ src/backend/ConnectionMySqlDB.java
@@ -0,0 +1,39 @@
+
+package backend;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionMySqlDB {
+	
+
+	/*** Attributes - Variables ***/ 
+	private static final String name_db = "home_link_db";
+	private static final String user_db = "root";
+	private static final String password_db = "nose@MySQL99";
+	private static final String host_server_db = "localhost";
+	private static final String port_server_db = "3306";
+	private static final String url_db = "jdbc:mysql://" + host_server_db + ":" + port_server_db + "/" + name_db;
+										// "jdbc:mysql://localhost:3306/home_link_db"
+	private ConnectionMySqlDB connection = null;
+	
+	
+	/* Method to get database connection */
+	public ConnectionMySqlDB getConnectionMySqlDB()
+	{
+		try {
+			connection = (ConnectionMySqlDB) DriverManager.getConnection(url_db, user_db, password_db);
+			if (connection != null) {
+				System.out.println("[success] Connected to the database: "+name_db);
+			}
+		} catch (SQLException e) {
+			System.out.println("[failed] An error occurred. Database Connection Failed!");
+			e.printStackTrace();
+		}
+		
+		return connection; //Return an instance of database connection
+	}
+	
+	
+
+}
--- src/backend/Main.java
+++ src/backend/Main.java
@@ -1,3 +1,4 @@
+
 package backend;
 
 import javafx.application.Application;
