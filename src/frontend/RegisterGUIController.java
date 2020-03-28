d512bedb3b68c2af98011d81880f975a41f6cab5
anthonysaka <48063344+anthonysaka@users.noreply.github.com>
2020-03-28 00:58:06 -0400

    f

--- src/frontend/RegisterGUIController.java
+++ src/frontend/RegisterGUIController.java
@@ -49,7 +49,6 @@ public class RegisterGUIController implements Initializable {
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
 		cbxGender.setItems(listGender);
 		cbxCategoryAccount.setItems(listTypeAccount);
 		
