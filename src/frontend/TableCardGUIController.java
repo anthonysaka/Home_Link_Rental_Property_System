7793d084652de8e0e34f59a2a36ec5726c437a39
Wendily <Wendily@Wendily>
2020-04-08 23:26:16 -0700

    hola

---
+++ src/frontend/TableCardGUIController.java
@@ -0,0 +1,34 @@
+package frontend;
+
+import java.awt.event.ActionEvent;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class TableCardGUIController {
+
+    @FXML
+    private JFXButton btnAddCard;
+
+    @FXML
+    private JFXButton btnModifyCard;
+
+    @FXML
+    private Button btnClose;
+
+    @FXML
+    private JFXButton btnDeleteCard;
+
+    
+ /*****************************METODOS**************************/    
+    @FXML
+    void AddNewCard(ActionEvent event) {
+
+    }
+
+    @FXML
+    void closeWindow(ActionEvent event) {
+
+    }
+}
