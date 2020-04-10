f9715840bd263bd6442e56e2c70d910fc90cbb30
Jhan <Jhan@Laptop_Jhan>
2020-04-10 19:17:25 -0400

    123

--- src/frontend/TableCardGUIController.java
+++ src/frontend/TableCardGUIController.java
@@ -1,12 +1,25 @@
 package frontend;
 
-import java.awt.event.ActionEvent;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
 
 import com.jfoenix.controls.JFXButton;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 
-public class TableCardGUIController {
+import backend.ConnectionMySqlDB;
+import backend.Tarjetas;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+public class TableCardGUIController implements Initializable {
 
     @FXML
     private JFXButton btnAddCard;
@@ -20,8 +33,20 @@ public class TableCardGUIController {
     @FXML
     private JFXButton btnDeleteCard;
 
+    @FXML
+    private TableColumn<Tarjetas, String> tablenombre;
+
+    @FXML
+    private TableColumn<?, ?> tablenumero;
+
+    @FXML
+    private TableColumn<?, ?> tablefecha;
+
+    @FXML
+    private TableColumn<?, ?> tablecvv;
     
- /*****************************METODOS**************************/    
+    ObservableList<String> data;
+
     @FXML
     void AddNewCard(ActionEvent event) {
 
@@ -31,4 +56,35 @@ public class TableCardGUIController {
     void closeWindow(ActionEvent event) {
 
     }
+    
+    public ObservableList<String> getcardData(){
+    	
+    		
+    		Statement sentencia = null;
+    		ResultSet resultado = null;
+    		ArrayList<String> lista = new ArrayList<String>();
+    		String Query = "SELECT  holder_fullname, num_card, expiration_date, cvv FROM t_card WHERE id ="+HomeGUIController.usuarioActual.getId();
+    		try {
+    			Connection myConnection = ConnectionMySqlDB.getConnectionMySqlDB();
+    			sentencia = myConnection.createStatement();
+    			resultado = sentencia.executeQuery(Query);
+    			
+    			while (resultado.next()) {
+    				data.addAll(resultado.getString("holder_fullname"), resultado.getString("num_card"), resultado.getString("expiration_date"), resultado.getString("cvv"));
+    				System.out.println("Datos de tarjetas guardadas obtenidos");
+    			}
+    			
+    		} catch (Exception e) {
+    			System.out.println("Error al cargar las tarjetas guardadas!");
+    		}
+    	return data;
+    }
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
+		//tablenombre.setCellFactory(new PropertyValueFactory<>(getcardData().get(1).toString());
+		
+	}
+
 }
